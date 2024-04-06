ccfb755e6fbd73de54824043da60eb71e500e820
medinat akinsowon <medinahakinsowon@gmail.com>
2024-04-06 10:07:26 +0100

    replaced the header image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css';
+import HeaderImage from '../../assets/header_img.png';
 
 const Header = () => {
   return (
     <div className='header'>
+     <img src={HeaderImage} className='header-image'/>
       <div className="header_contents">
-        <h2>Order Your Favourite Food Here</h2>
+        <h2>Order Your Favourite Food Here.</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo earum eius officia tempora impedit deleniti sit quos repellendus aspernatur assumenda.</p>
         <button>View Menu</button>
       </div>
